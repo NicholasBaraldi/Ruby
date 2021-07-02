a9d9bc4eb56b8dbfc3b8e86c578e495f71a8d875
NicholasBaraldi <nicholas.baraldi@gmail.com>
2021-07-02 02:41:11 -0300

    refactoring Rock, Paper, Scissors

--- Rock_Paper_Scissors.rb
+++ Rock_Paper_Scissors.rb
@@ -1,25 +1,100 @@
-player_move = ["rock", "paper", "scissors"].sample
-computer_move = ["rock", "paper", "scissors"].sample
+class Player
+    MOVES = [:rock, :paper, :scissors]
 
-win_scenarios = [
-    ["rock", "scissors"],
-    ["paper", "rock"],
-    ["scissors", "paper"]
+    attr_reader :score, :move
+
+    def initialize
+        @score = 0
+        @move = nil
+    end
+
+    def get_move
+        loop do
+            puts "Pick a move."
+            print "> "
+            @move = gets.chomp.strip.downcase.to_sym
+            if @move == :quit
+                return false
+            elsif MOVES.include?(@move)
+                return @move
+            else
+                puts "Invalid move."
+            end
+        end
+    end
+
+    def win
+        @score += 1
+    end
+end
+
+class PC_Player < Player
+
+    def get_move
+        @move = MOVES.sample
+    end
+
+end
+
+class Game
+
+    WIN_SCENARIOS = [
+    [:rock, :scissors],
+    [:paper, :rock],
+    [:scissors, :paper]
 ]
 
-lose_scenarios = [
-    ["rock", "paper"],
-    ["paper", "scissors"],
-    ["scissors", "rock"]
-]
+    def initialize
+        @tie = 0
+        @p1 = Player.new
+        @p2 = PC_Player.new
+        play
+    end
 
-puts(
-if player_move == computer_move
-    "It's a tie!"
-elsif win_scenarios.include?([player_move, computer_move])
-    "You Win!"
-elsif lose_scenarios.include?([player_move, computer_move])
-    "You Lose!"
-else
-    "Unexpected input"
-end)
+    def winner
+        if @p1.move == @p2.move
+            puts "It's a tie!"
+            @tie += 1
+        elsif WIN_SCENARIOS.include?([@p1.move, @p2.move])
+            puts "You win!"
+            @p1.win
+        else
+            puts "PC wins!"
+            @p2.win
+        end
+    end
+
+    def round
+        return false unless @p1.get_move
+        return false unless @p2.get_move
+        result
+        puts
+        scores
+        puts
+        puts "Let's play again."
+        true
+    end
+
+    def play
+        loop do
+            break unless round
+        end
+        puts
+        puts "Goodbye!"
+    end
+
+    def result
+        puts
+        puts "You played #{@p1.move} and PC played #{@p2.move}."
+        winner
+    end
+
+    def scores
+        puts "Scores:"
+        puts "Player: #{@p1.score}"
+        puts "PC: #{@p2.score}"
+        puts "Ties: #{@tie}"
+    end
+end
+
+Game.new
